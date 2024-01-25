c5f5673aae910b25522ad8ff1fd4da03ecddff68
walter-cripto <112146520+walter-cripto@users.noreply.github.com>
2024-01-24 23:37:53 -0300

    texto.hpp

--- texto.hpp
+++ texto.hpp
@@ -9,9 +9,29 @@
 // 
 
 #include<fstream>
+#include<iostream>
 
 void escribir(std::string nombre_archivo,std::string lineas_escritura =""){
     std::ofstream archivo(nombre_archivo, std::ios::app);
+    if(archivo.is_open()){
     archivo << lineas_escritura <<"\n";
     archivo.close();
+    }else{
+        std::cerr<<"Error al abrir el archivo";
+    }
+}
+
+void leer(std::string nombre_archivo) {
+    std::ifstream archivo(nombre_archivo);
+    std::string linea;
+    if (archivo.is_open()) {
+        
+        while (std::getline(archivo, linea)) {
+            std::cout << linea << std::endl;
+        }
+        archivo.close();
+    } else {
+        std::cout << "No se pudo abrir el archivo." << std::endl;
+    }
+    
 }
